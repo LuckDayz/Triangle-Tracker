924be3160257426962596170417a6e5c1ddde416
Dayo <adejomoemma@gmail.com>
2023-08-17 21:12:07 -0700

    Triangle Tracker

---
+++ README.md
@@ -0,0 +1,29 @@
+Triangle Tracker
+
+A triangle's properties and measurements are calculated using various mathematical principles in the form field.
+
+By Adedayo Adejumo
+
+*Technologies Used*
+GIT
+HTML5
+CSS
+BOOTSTRAP
+Javascript and jQuery
+
+*Description*
+A triangle's properties and measurements are calculated using various mathematical principles in the form field.
+
+*Setup/Installation Requirements*
+Clone this repository to your desktop
+Navigate to the top level of the directory
+Open the index.html file in your browser.
+
+*Known Bugs*
+None Known
+
+*License*
+MIT
+
+Contact Information
+adejomoemma@gmail.com
--- js/script.js
+++ js/script.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
         event.preventDefault();
         let side1 = parseInt($("input#side1").val());
         let side2 = parseInt($("input#side2").val());
-        let side3 = parseInt($("input#side2").val());
+        let side3 = parseInt($("input#side3").val());
 
         if(side1 === side2 && side2 === side3 ){
             $("#result").text("It's an Equilateral Triangle");
-        }else if(side1 <= side2 || (side2 > side3)){
+        }else if(side1 === side2 || side1 === side3 ){
             $("#result").text("It's an Isosceles Triangle");
+        }else if((side1 <= side2 || side1 <= side3) ){
+            $("#result").text("Not a triangle");
         }else{
-            $("#result").text("It's Scalene triangle");
+            $("#result").text("It's a Scalene triangle");
         }
     });
 });
